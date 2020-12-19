3f7bceebd340f25beadd49f473aaeee943e578dd
Muntarin <54241709+Muntarin@users.noreply.github.com>
2020-12-19 17:59:02 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function changeSign(input){
+    if(calculator.display.value.substring(0,1)=="-")
+     calculator.display.value=calculator.display.value.substring(1,calculator.display.value.length)
+
+    else
+     calculator.display.value="-"+calculator.display.value
+}
